b0a112fe44080f0431d84aeb522c74d1b0ab7a26
afra959 <xujn416387681@gmail.com>
2026-09-21 18:36:04 +0800

    Add music notification listener service

---
+++ music_native/app/src/main/java/com/afra/music/MusicNotificationListenerService.java
@@ -0,0 +1,10 @@
+package com.afra.music;
+
+import android.service.notification.NotificationListenerService;
+
+public class MusicNotificationListenerService extends NotificationListenerService {
+    @Override
+    public void onListenerConnected() {
+        super.onListenerConnected();
+    }
+}
