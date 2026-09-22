eea656489a66938c3d0cc7d319ca8434adbf724f
afra959 <xujn416387681@gmail.com>
2026-09-22 09:06:28 +0800

    Keep listener instance for notification fallback

--- music_native/app/src/main/java/com/afra/music/MusicNotificationListenerService.java
+++ music_native/app/src/main/java/com/afra/music/MusicNotificationListenerService.java
@@ -3,8 +3,18 @@ package com.afra.music;
 import android.service.notification.NotificationListenerService;
 
 public class MusicNotificationListenerService extends NotificationListenerService {
+
+    public static volatile MusicNotificationListenerService instance;
+
     @Override
     public void onListenerConnected() {
         super.onListenerConnected();
+        instance = this;
+    }
+
+    @Override
+    public void onListenerDisconnected() {
+        instance = null;
+        super.onListenerDisconnected();
     }
 }
