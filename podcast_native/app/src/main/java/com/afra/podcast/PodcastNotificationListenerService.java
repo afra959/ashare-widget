fe4833904417a2c5c21a2e5ce701c66d512b6016
afra959 <xujn416387681@gmail.com>
2026-09-22 09:23:44 +0800

    Add 播播 notification listener

---
+++ podcast_native/app/src/main/java/com/afra/podcast/PodcastNotificationListenerService.java
@@ -0,0 +1,10 @@
+package com.afra.podcast;
+
+import android.service.notification.NotificationListenerService;
+
+public class PodcastNotificationListenerService extends NotificationListenerService {
+    @Override
+    public void onListenerConnected() {
+        super.onListenerConnected();
+    }
+}
